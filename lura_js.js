7e3cc389090ee393e1ff9973f5e7138223a3ef2a
cengsemihsahin <esemihsahin@hotmail.com>
2021-12-23 11:56:30 +0300

    lura dropdown menu

--- lura_js.js
+++ lura_js.js
@@ -25,6 +25,8 @@ $(document).ready(function() {
   // Flexscroll div fade in (drop-down menu)
   $('.Flexscroll').addClass('fade-in-ferisoft');
 
+  $('.Flexscroll').append('<img src="https://lura.ticimaxeticaret.com/Uploads/UrunResimleri/buyuk/lura-temizlik-robotu-f-488f.jpg" style="height: 350px; float: right;">');
+
   // Drop-down menu link icon ---------------------------------------------------------------------------
   var dropdownSvg = '<svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="bi bi-caret-down" viewBox="0 0 16 16">' +
                       '<path d="M3.204 5h9.592L8 10.481 3.204 5zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659z"/>' +
