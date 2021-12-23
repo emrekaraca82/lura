5ac3b9bdd6387e37353b2fc93e465b298789cf36
cengsemihsahin <esemihsahin@hotmail.com>
2021-12-23 11:28:12 +0300

    dropdown menu icon

--- lura_js.js
+++ lura_js.js
@@ -22,7 +22,17 @@ $(document).ready(function() {
   $('title').text('Lura');
   $('body').prepend('<div id="mist_div"></div>');
 
+  // Flexscroll div fade in (drop-down menu)
   $('.Flexscroll').addClass('fade-in-ferisoft');
+
+  // Drop-down menu link icon ---------------------------------------------------------------------------
+  var dropdownSvg = '<svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="bi bi-caret-down" viewBox="0 0 16 16">' +
+                      '<path d="M3.204 5h9.592L8 10.481 3.204 5zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659z"/>' +
+                    '</svg>'
+    
+  // console.log($('.ulVar a').first().text());
+  $('.ulVar a').first().html($('.ulVar a').first().text() + '&nbsp;&nbsp;' + dropdownSvg)
+  // ----------------------------------------------------------------------------------------------------
 
   $('.ulVar').hover(function () {
     // Site mist | Semih SAHIN - FERISOFT
